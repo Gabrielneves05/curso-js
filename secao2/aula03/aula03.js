3e80c08857e15c4ec49859ddeb929d32048aa8e8
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2023-10-23 18:21:58 -0300

    Aula 03 - 23/10
    Revisão de aulas:
    
    Console.log - A primeira coisa que você vai ver em JS

---
+++ secao2/aula03/aula03.js
@@ -0,0 +1,7 @@
+// Console.log - A primeira coisa que você vai ver em JS
+
+console.log("Gabriel 'Neves'"); // String = Texto
+console.log('Gabriel "Neves"');
+console.log(`'Gabriel' "Neves"`);
+
+console.log(123456, 15.85, 'Gabriel Neves');
