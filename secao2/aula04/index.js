cc8849f10b68089d0f3d82b600d28e63e8562231
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2023-11-06 21:26:33 -0300

    Aula 04 - Comentários de código (06/11/23)
    Revisando:
    
    Comentários em Javascript

---
+++ secao2/aula04/index.js
@@ -0,0 +1,12 @@
+// Comentários de código
+
+// "// Teste" = Comentário de linha
+// "/* Teste */" = Comentário de bloco
+
+console.log('Olá mundo!'); // Aqui tem um comentário
+console.log('Linha 1');
+/*
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+*/
